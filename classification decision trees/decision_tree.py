39a5f1d9ccab8eb9a133685d3fe014817a220ceb
coding-hamster <36016000+coding-hamster@users.noreply.github.com>
2018-04-20 20:12:05 +0500

    Add files via upload

---
+++ classification decision trees/decision_tree.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+    Use a Decision Tree to identify emails from the Enron corpus by author:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+sys.path.append("D://PycharmProjects//untitled//cкрипты Python//udacity_intro_to_machine_learning//tools")
+from email_preprocess import preprocess
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+t0 = time()
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+predicted_values = clf.predict(features_test)
+accuracy = accuracy_score(labels_test, predicted_values)
+number_of_features = len(features_train[0])
+print(accuracy)
+print("training time:", round(time()-t0, 3), "s")
+print(number_of_features)
+
+
+
+#########################################################
+### your code goes here ###
+
+
+#########################################################
+
